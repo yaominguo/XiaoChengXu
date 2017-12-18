c4e5169e6b7017ab5ce075cb7e15e033f9bec836
yaominguo <missgmy@yahoo.com>
2017-12-18 23:47:24 +0800

    构建检查数独的解决方案

--- SudokuGame/pages/index/index.js
+++ SudokuGame/pages/index/index.js
@@ -46,6 +46,30 @@ let handler = {
       showPopupNumber: false
     })
   },
+  // check() {
+  //   const $rows = this._$container.children();
+  //   const data = $rows.map((rowIndex, div) => {
+  //     return $(div).children().map((colIndex, span) => {
+  //       return parseInt($(span).text()) || 0;
+  //     })
+  //   }).toArray().map($data => $data.toArray());
+  //   const checker = new Checker(data);
+  //   if (checker.check()) {
+  //     return true;
+  //   }
+  //   //检查不成功，进行标记
+  //   const marks = checker.matrixMarks;
+  //   this._$container.children().each((rowIndex, div) => {
+  //     $(div).children().each((colIndex, span) => {
+  //       const $span = $(span);
+  //       if ($span.is('.fixed') || marks[rowIndex][colIndex]) {
+  //         $span.removeClass('error');
+  //       } else {
+  //         $span.addClass('error');
+  //       }
+  //     })
+  //   })
+  // },
   reset() {
     this.setData({
       dataSource: this.data.modelDataSource
